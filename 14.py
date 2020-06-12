2d1adb76db094df57b4bf8575ed6f62e50cfe46f
HenryM975 <henry975@yandex.ru>
2020-06-12 17:40:27 +0300

    2nd

--- 14.py
+++ 14.py
@@ -1,17 +1,26 @@
-i = 13
-list1 = []
-list2 = ()
-for i in range(1,10):
-    while True:
-        print(i)
-        list1.append(i)
-        len_list = len(list1)
-        if i%2 == 0:
-            i /= 2
-        elif i <= 1:
-            break
-            list2.append(list1)
-            list2.append("*")
-        else:
-            i = 3*i + 1
-print(list2)
+try:
+    first_num = int(input("first num: "))
+    last_num = int(input("last_num: "))
+    maxlen = 0
+    for i in range(first_num, last_num + 1):
+        list1 = []
+        startnum = i
+        print("***")
+        while True:
+            print(i)
+            list1.append(i)
+            if i % 2 == 0:
+                i /= 2
+            elif i <= 1:
+                if len(list1) > maxlen:
+                    maxlen = len(list1)
+                    num = startnum
+                break
+            else:
+                i = 3 * i + 1
+    print("maxlen:")
+    print(maxlen)
+    print("startnum:")
+    print(num)
+except ValueError:
+    print("write the number")
