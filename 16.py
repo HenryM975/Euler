1e70f1d7fc7dbb0d4ae06b6e4a818bf01f528421
HenryM975 <henry975@yandex.ru>
2020-06-13 17:41:41 +0300

    3rd

---
+++ 16.py
@@ -0,0 +1,17 @@
+while True:
+    print("a^n")
+    try:
+        a = int(input("a: "))
+        b = int(input("b: "))
+        num = a**b
+        print(num)
+        num_list = list(str(num))
+        Sum_num = 0
+        for i in num_list:
+            i = int(i)
+            Sum_num+=i
+        print("sum: ", end="")
+        print(Sum_num)
+        break
+    except ValueError:
+        print("write the number")
