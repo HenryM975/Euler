1d1345dfed23d79664607211cc4dd997d0ea0ffc
HenryM975 <henry975@yandex.ru>
2020-06-23 17:09:32 +0300

    11th

--- 22.py
+++ 22.py
@@ -2,6 +2,7 @@
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 alphabet_dict1 = {}
 alphabet_dict2 = {}
+Final_dict = {}
 num = 0
 for i in alphabet:
     num += 1
@@ -14,18 +15,16 @@ for letter in alphabet_dict2:
 #make dict with "a":1 "A":1 connection of two pronounced dictionary
 open_file = open('22data')
 rf = open_file.read()
-print(rf)
 name_list = rf.split(",")
-print(name_list)
 name_list = sorted(name_list)
-print(name_list)
-print(alphabet_dict1)
-print(alphabet_dict2)
 for name in name_list:
     name_letters = list(name)
-print(name_letters)
-for name_letter in name_letters:
-    for part in alphabet_dict1:
-        if name_letter == part:
-            print(part)
-            print(alphabet_dict1[part])
+    letters_num_sum = 0
+    for name_letter in name_letters:
+        for part in alphabet_dict1:
+            if name_letter == part:
+                letters_num_sum += alphabet_dict1[part]
+    name_index = name_list.index(name) + 1
+    final_num = letters_num_sum * name_index
+    Final_dict[name] = final_num
+print(Final_dict)
