950df5728318f4715cfb7becee643cb261db21ac
HenryM975 <henry975@yandex.ru>
2020-08-10 23:13:57 +0300

    23rd

--- 32.py
+++ 32.py
@@ -1,6 +1,6 @@
 #32
 finallist = []
-for num in range(30):
+for num in range(100000):
     numlist = list(str(num))
     lenlist = len(numlist)
     numlist.sort()
@@ -14,5 +14,42 @@ for num in range(30):
     if checklist == numlist:
             finallist.append(num)
 print(finallist)
-    
+#?
+for first in range(1, 100):
+    for second in range(1, 200):
+        for third in range(1, 10000):
+            split_list = []
+            if first * second == third:
+                split_list.append(first)
+                split_list.append(second)
+                split_list.append(third)
+                #print(split_list)
+                sf_list = []
+                for part in split_list:
+                    for element in str(part):
+                        sf_list.append(element)
+                #print(sf_list)
+                num_len = len(sf_list)
+                int_sf_list = []
+                for element in sf_list:
+                    int_element = int(element)
+                    int_sf_list.append(int_element)
+                sorted_int_sf_list = sorted(int_sf_list)
+                list_of_len_sf_list = []
+                for len_element_part in range(len(sf_list)):
+                    list_of_len_sf_list.append(len_element_part)
+                #print(sorted_int_sf_list)
+                #print(list_of_len_sf_list)
+                print("-----------------")
+                if sorted_int_sf_list == list_of_len_sf_list:
+                    print(first)
+                    print(second)
+                    print(third)
+                    print("+++++++++++")
+
+
+
+
+
+
         
---
+++ 33.py
@@ -0,0 +1,7 @@
+#33
+for numerator in range(1, 100):
+    for denominator in range(1, 100):
+        fraction = float(numerator/denominator)#???
+        if fraction < 1 and fraction > 0:
+            print(fraction)
+
