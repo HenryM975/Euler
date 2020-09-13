c07e58ebd734e93b5fb6c8e8ae3acb38920848b8
HenryM975 <henry975@yandex.ru>
2020-09-13 13:50:24 +0300

    31st

--- 38.py
+++ 38.py
@@ -1,16 +1,18 @@
 #38
 comparison_list_main = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-for num in range(200):    
+final_list = []
+for num in range(100000):
+    print(num)
     testlist = []
     comparison_list_local = []
-    for factor  in range(1, 4):
+    for factor  in range(1, 100):
         composition = num * factor
         testlist.append(composition)
-    r = "".join(map(str, testlist))
-    print(r)
-    print(testlist)
-    for element in r:
-        comparison_list_local.append(int(element))
-    if sorted(comparison_list_local) == comparison_list_main:
-        print("fine!")
-
+        r = "".join(map(str, testlist))
+        if len(r) == 9:
+            for element in r:
+                comparison_list_local.append(int(element))
+            if sorted(comparison_list_local) == comparison_list_main:
+                final_list.append(num)
+            break
+print(final_list)
